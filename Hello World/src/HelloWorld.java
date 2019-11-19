27018626f05ca91606e31192e0be08250d694a70
최상희 <@DESKTOP-2R4S3CT>
2019-11-19 17:51:26 +0900

    hotfix branch change a code

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("WKU");
 		System.out.println("2'nd grade 최상희");
+		System.out.println("이클립스 과제");
+
 		// TODO Auto-generated method stub
 
 	}
