c1a3ee2399da2fa95ab084ae27c46220041fb5f5
최상희 <@DESKTOP-2R4S3CT>
2019-11-19 17:40:31 +0900

    added print line

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("WKU");
+		System.out.println("2'nd grade √÷ªÛ»Ò");
 		// TODO Auto-generated method stub
 
 	}
