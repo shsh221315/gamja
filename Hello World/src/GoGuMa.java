164810898954af693a35ec39b33b84d4b2fa2e1d
shsh221315 <55828245+shsh221315@users.noreply.github.com>
2019-11-19 18:46:35 +0900

    PushPrint.java modified on GitHub

--- Hello World/src/GoGuMa.java
+++ Hello World/src/GoGuMa.java
@@ -3,7 +3,7 @@ public class GoGuMa {
 
 	public static void main(String[] args) {
 		System.out.println("fried");
-		System.out.println("is good");
+		System.out.println("food");
 
 		// TODO Auto-generated method stub
 
