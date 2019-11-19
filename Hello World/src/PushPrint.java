5e86b4225b12f2a4c013c62c426e9a29171adfe2
최상희 <@DESKTOP-2R4S3CT>
2019-11-19 19:12:23 +0900

    Merge remote-tracking branch 'orgin/master' by shsh221315
    Conflicts:
    	HelloWorld/src/PushPrint.java

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -3,11 +3,8 @@ public class PushPrint {
 
 	public static void main(String[] args) {
 		System.out.println("gamjaa");
-<<<<<<< HEAD
 		System.out.println("foooood");
-=======
 		System.out.println("goooood");
->>>>>>> refs/remotes/origin/master
 		// TODO Auto-generated method stub
 
 	}
